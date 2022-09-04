375d377e0e32a0c472afb5e13ad946b9ca0fb89b
grumarg <grushevskayyy7@gmail.com>
2022-09-04 23:59:10 +0300

    fixed positioning in header & start creat HomePage

--- src/componets/pages/Home.js
+++ src/componets/pages/Home.js
@@ -1,9 +1,26 @@
+import brain from '../../assets/images/brain.png';
+
 import '../../assets/styles/css/pages/home.css';
 
 function Home() {
-  return(
-    <main> 
-    </main>
+  return (
+    <div className='main_skeleton'>
+      <div className='main_info'>
+        <div><h6>Complex Analytics For Simpler Urban Life</h6></div>
+        <div className='title'><h1>URBAN COMPLEXITY LAB</h1></div>
+        <div className='about'> 
+          <h5>
+            We promote potential of data science and complexity to broad audience and the students through teaching and public speaking.
+          </h5>
+          <h5>
+            Our courses include Applied Data Science, Machine Learning, Network Analysis, Scientific Writing.
+          </h5> 
+        </div>
+      </div>
+      <div>
+        <img src={brain} alt="brain"/>
+      </div>
+    </div>
   );
 }
 
